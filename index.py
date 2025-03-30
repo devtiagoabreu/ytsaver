576a3dac78d71a8230ae2f3faaaa8739662df97f
devtiagoabreu <devtiagoabreu@gmail.com>
2025-03-30 16:36:40 -0300

    inserindo deploy na vercel

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import os
 import re
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template
 from pytubefix import YouTube
 from moviepy.editor import VideoFileClip
 from youtube_transcript_api import YouTubeTranscriptApi
@@ -10,30 +10,24 @@ app = Flask(__name__)
 def limpar_nome(nome):
     return re.sub(r'[^a-zA-Z0-9_-]', '_', nome)
 
-# Função para tratar o progresso do download
-def progresso_callback(stream, chunk, bytes_remaining):
-    total_size = stream.filesize
-    bytes_downloaded = total_size - bytes_remaining
-    progresso = (bytes_downloaded / total_size) * 100
-    return progresso
-
 def baixar_video(url, pasta):
     yt = YouTube(url)
     titulo_limpo = limpar_nome(yt.title)
     video_path = os.path.join(pasta, f"{titulo_limpo}.mp4")
     audio_path = os.path.join(pasta, f"{titulo_limpo}.mp3")
     
-    # Obtém o stream de maior resolução
+    # Baixando o vídeo na maior resolução
     video_stream = yt.streams.get_highest_resolution()
-    
-    # Baixando o vídeo com monitoramento do progresso
     video_stream.download(output_path=pasta, filename=f"{titulo_limpo}.mp4")
     
-    # Extrai o áudio do vídeo
+    # Convertendo vídeo para áudio
     video = VideoFileClip(video_path)
     video.audio.write_audiofile(audio_path)
     video.close()
     
+    # Baixando a transcrição
+    obter_transcricao(url, pasta, titulo_limpo)
+
     return titulo_limpo
 
 def obter_transcricao(url, pasta, titulo):
@@ -54,16 +48,25 @@ def index():
         desktop = os.path.join(os.path.expanduser("~"), "Desktop")
         pasta = os.path.join(desktop, "Videos_Baixados")
         os.makedirs(pasta, exist_ok=True)
-        
-        # Realiza o download e transcrição
-        titulo = baixar_video(url, pasta)
-        obter_transcricao(url, pasta, titulo)
-        
-        return render_template("index.html", pasta=pasta, titulo=titulo)
-    
-    return render_template("index.html", pasta=None)
 
-@app.route("/reset", methods=["GET"])
+        # Exibe a mensagem de "Aguarde" após o envio do formulário
+        return render_template("index.html", pasta=pasta, aguardando=True, url=url)
+
+    return render_template("index.html", aguardando=False)
+
+@app.route("/processar", methods=["POST"])
+def processar():
+    url = request.form["url"]
+    desktop = os.path.join(os.path.expanduser("~"), "Desktop")
+    pasta = os.path.join(desktop, "Videos_Baixados")
+    os.makedirs(pasta, exist_ok=True)
+
+    # Baixa o vídeo, áudio e transcrição
+    titulo = baixar_video(url, pasta)
+
+    return render_template("index.html", pasta=pasta, titulo=titulo, url=url)
+
+@app.route("/reset", methods=["POST"])
 def reset():
     return render_template("index.html", pasta=None)
 
