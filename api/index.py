b663b950ee5e1837d150b7b500045554dd3abfd0
devtiagoabreu <devtiagoabreu@gmail.com>
2025-03-31 23:09:43 -0300

    limpando o frontend

--- api/index.py
+++ api/index.py
@@ -57,9 +57,9 @@ def obter_transcricao(url, pasta, titulo):
         with open(resumo_path, "w", encoding="utf-8") as f:
             f.write(resumo)
 
-        # Gerar e salvar Reunião empresarial
-        resumo = criar_reuniao_empresarial_da_transcricao(texto)
-        resumo_path = os.path.join(pasta, f"{titulo}_reuniao_empresarial.txt")
+         # Gerar e salvar contúdo para tabnews
+        resumo = gerar_conteudo_para_tabnews_da_transcricao(texto)
+        resumo_path = os.path.join(pasta, f"{titulo}_conteudo_tabnews.txt")
         with open(resumo_path, "w", encoding="utf-8") as f:
             f.write(resumo)
 
@@ -69,12 +69,12 @@ def obter_transcricao(url, pasta, titulo):
         with open(resumo_path, "w", encoding="utf-8") as f:
             f.write(resumo)
 
-         # Gerar e salvar contúdo para tabnews
-        resumo = gerar_conteudo_para_tabnews_da_transcricao(texto)
-        resumo_path = os.path.join(pasta, f"{titulo}_conteudo_tabnews.txt")
+        # Gerar e salvar Reunião empresarial
+        resumo = criar_reuniao_empresarial_da_transcricao(texto)
+        resumo_path = os.path.join(pasta, f"{titulo}_reuniao_empresarial.txt")
         with open(resumo_path, "w", encoding="utf-8") as f:
             f.write(resumo)
-
+        
     except Exception as e:
         print(f"Erro ao obter a transcrição: {e}")
 
@@ -84,12 +84,22 @@ def carregar_prompt():
     if os.path.exists(prompt_path):
         with open(prompt_path, "r", encoding="utf-8") as f:
             return f.read().strip()
-    return "Resuma este texto em 3 partes: 1) Descrição clara (3000 chars); 2) Tópicos-chave em tópicos; 3) Plano de estudo com fontes (livros, artigos). Mantenha acadêmico e direto, no final crie mais um tópico como se fosse um post em um blog especializado sobre o assunto, com estudos sobre o assunto e com emojis para ficar bonito, crie tags para ligar informações no obsidian logo no final de acordo com o resumo gerado:"  # Caso o arquivo não exista
+    return "Resuma este texto em 3 partes: 1) Descrição clara (3000 chars); 2) Tópicos-chave em tópicos; 3) Plano de estudo com fontes (livros, artigos). Mantenha acadêmico e direto, no final crie mais um tópico como se fosse um post em um blog especializado sobre o assunto, com estudos sobre o assunto e com emojis para ficar bonito, crie tags para ligar informações no obsidian logo no final de acordo com o resumo gerado, Título criativo** (usar emojis e trocadilhos, ex: 'Async/Await: O Herói Sem Capa do JavaScript', Formato Markdown:"  # Caso o arquivo não exista
 
 def resumir_transcricao(texto):
     """Gera um resumo da transcrição com um prompt personalizado."""
     try:
         prompt = carregar_prompt()
+        model = genai.GenerativeModel("gemini-1.5-pro-latest")
+        response = model.generate_content(f"{prompt}\n\n{texto}")
+        return response.text
+    except Exception as e:
+        return f"Erro ao gerar resumo: {str(e)}"
+
+def gerar_conteudo_para_tabnews_da_transcricao(texto):
+    """Gera um resumo da transcrição com um prompt personalizado."""
+    try:
+        prompt = "Transforme esta transcrição de vídeo em um **artigo didático para o TabNews**, mas o conteúdo precisa parecer orinal, escrito por um programador experiente, seguindo o estilo Filipe Deschamps, Tom Descontraído, mas técnico e Direto ao ponto, com piadinhas nerds e analogias fáceis de entender (ex: 'Isso é tipo um `console.log` da vida real') - Título criativo** (usar emojis e trocadilhos, ex: 'Async/Await: O Herói Sem Capa do JavaScript', Formato Markdown, Tá, mas como funciona? Tópicos técnicos em **lista simples**, com código snippets (se aplicável), Exemplo prático, Ênfase em **'por que isso importa'** (ex: 'Isso evita aquele loop infernal de callbacks!') Dica Pro Insights úteis que não estão óbvios no vídeo) Bônus: Links Úteis [Documentação]() | [Ferramenta]() | [Artigo Relacionado]() , crie tags para ligar informações no obsidian logo no final de acordo com o resumo gerado"
         model = genai.GenerativeModel("gemini-1.5-pro-latest")
         response = model.generate_content(f"{prompt}\n\n{texto}")
         return response.text
@@ -116,16 +126,6 @@ def gerar_conteudo_para_rede_social_da_transcricao(texto):
     except Exception as e:
         return f"Erro ao gerar resumo: {str(e)}"
     
-def gerar_conteudo_para_tabnews_da_transcricao(texto):
-    """Gera um resumo da transcrição com um prompt personalizado."""
-    try:
-        prompt = "Transforme esta transcrição de vídeo em um **artigo didático para o TabNews**, seguindo o estilo Filipe Deschamps, Tom Descontraído, mas técnico e Direto ao ponto, com piadinhas nerds e analogias fáceis de entender (ex: 'Isso é tipo um `console.log` da vida real') - Título criativo** (usar emojis e trocadilhos, ex: 'Async/Await: O Herói Sem Capa do JavaScript', Formato Markdown, Tá, mas como funciona? Tópicos técnicos em **lista simples**, com código snippets (se aplicável), Exemplo prático, Ênfase em **'por que isso importa'** (ex: 'Isso evita aquele loop infernal de callbacks!') Dica Pro Insights úteis que não estão óbvios no vídeo) Bônus: Links Úteis [Documentação]() | [Ferramenta]() | [Artigo Relacionado]() , crie tags para ligar informações no obsidian logo no final de acordo com o resumo gerado"
-        model = genai.GenerativeModel("gemini-1.5-pro-latest")
-        response = model.generate_content(f"{prompt}\n\n{texto}")
-        return response.text
-    except Exception as e:
-        return f"Erro ao gerar resumo: {str(e)}"
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
